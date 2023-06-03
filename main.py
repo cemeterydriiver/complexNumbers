cb3def9cc1903796333ae1e5fb4c5d5c2f511264
ash <ash@debian>
2023-06-03 18:59:39 -0300

    add graphic interface

--- main.py
+++ main.py
@@ -1,98 +1,127 @@
-def exibir_menu():
-    print("===== Calculadora de Números Complexos =====")
-    print("Selecione a operação desejada:")
-    print("1 - Adição")
-    print("2 - Subtração")
-    print("3 - Multiplicação")
-    print("4 - Divisão")
-    print("5 - Sair")
+import tkinter as tk
+from tkinter import ttk, messagebox
 
-def adicionar(num1, num2):
-    resultado = num1 + num2
-    return resultado
-
-def subtrair(num1, num2):
-    resultado = num1 - num2
-    return resultado
-
-def multiplicar(num1, num2):
-    resultado = num1 * num2
-    return resultado
-
-def dividir(num1, num2):
+def somar():
     try:
-        resultado = num1 / num2
-        return resultado
-    except ZeroDivisionError:
-        print("Erro: Divisão por zero não é permitida!")
-        return None
+        real1 = float(entry_real1.get())
+        imag1 = float(entry_imag1.get())
+        num1 = complex(real1, imag1)
 
-def main():
-    sair = False
+        real2 = float(entry_real2.get())
+        imag2 = float(entry_imag2.get())
+        num2 = complex(real2, imag2)
 
-    while not sair:
-        exibir_menu()
-        escolha = input("Digite o número da operação desejada: ")
+        resultado = num1 + num2
+        messagebox.showinfo("Resultado", f"Resultado da adição: {resultado}")
+    except ValueError:
+        messagebox.showerror("Erro", "Entrada inválida!")
 
-        if escolha == "1":
-            real1 = float(input("Digite a parte real do primeiro número complexo: "))
-            imag1 = float(input("Digite a parte imaginária do primeiro número complexo: "))
-            num1 = complex(real1, imag1)
+def subtrair():
+    try:
+        real1 = float(entry_real1.get())
+        imag1 = float(entry_imag1.get())
+        num1 = complex(real1, imag1)
 
-            real2 = float(input("Digite a parte real do segundo número complexo: "))
-            imag2 = float(input("Digite a parte imaginária do segundo número complexo: "))
-            num2 = complex(real2, imag2)
+        real2 = float(entry_real2.get())
+        imag2 = float(entry_imag2.get())
+        num2 = complex(real2, imag2)
 
-            resultado = adicionar(num1, num2)
-            print("Resultado da adição:", resultado)
+        resultado = num1 - num2
+        messagebox.showinfo("Resultado", f"Resultado da subtração: {resultado}")
+    except ValueError:
+        messagebox.showerror("Erro", "Entrada inválida!")
 
-        elif escolha == "2":
-            real1 = float(input("Digite a parte real do primeiro número complexo: "))
-            imag1 = float(input("Digite a parte imaginária do primeiro número complexo: "))
-            num1 = complex(real1, imag1)
+def multiplicar():
+    try:
+        real1 = float(entry_real1.get())
+        imag1 = float(entry_imag1.get())
+        num1 = complex(real1, imag1)
 
-            real2 = float(input("Digite a parte real do segundo número complexo: "))
-            imag2 = float(input("Digite a parte imaginária do segundo número complexo: "))
-            num2 = complex(real2, imag2)
+        real2 = float(entry_real2.get())
+        imag2 = float(entry_imag2.get())
+        num2 = complex(real2, imag2)
 
-            resultado = subtrair(num1, num2)
-            print("Resultado da subtração:", resultado)
+        resultado = num1 * num2
+        messagebox.showinfo("Resultado", f"Resultado da multiplicação: {resultado}")
+    except ValueError:
+        messagebox.showerror("Erro", "Entrada inválida!")
 
-        elif escolha == "3":
-            real1 = float(input("Digite a parte real do primeiro número complexo: "))
-            imag1 = float(input("Digite a parte imaginária do primeiro número complexo: "))
-            num1 = complex(real1, imag1)
+def dividir():
+    try:
+        real1 = float(entry_real1.get())
+        imag1 = float(entry_imag1.get())
+        num1 = complex(real1, imag1)
 
-            real2 = float(input("Digite a parte real do segundo número complexo: "))
-            imag2 = float(input("Digite a parte imaginária do segundo número complexo: "))
-            num2 = complex(real2, imag2)
+        real2 = float(entry_real2.get())
+        imag2 = float(entry_imag2.get())
+        num2 = complex(real2, imag2)
 
-            resultado = multiplicar(num1, num2)
-            print("Resultado da multiplicação:", resultado)
+        denominador_conjugado = num2.conjugate()
+        numerador = num1 * denominador_conjugado
+        denominador = num2 * denominador_conjugado
 
-        elif escolha == "4":
-            real1 = float(input("Digite a parte real do primeiro número complexo: "))
-            imag1 = float(input("Digite a parte imaginária do primeiro número complexo: "))
-            num1 = complex(real1, imag1)
+        resultado = numerador / denominador
+        messagebox.showinfo("Resultado", f"Resultado da divisão: {resultado}")
+    except (ValueError, ZeroDivisionError):
+        messagebox.showerror("Erro", "Entrada inválida ou divisão por zero!")
 
-            real2 = float(input("Digite a parte real do segundo número complexo: "))
-            imag2 = float(input("Digite a parte imaginária do segundo número complexo: "))
-            num2 = complex(real2, imag2)
+window = tk.Tk()
+window.title("Calculadora de Números Complexos")
 
-            # Cálculo da divisão
-            denominador_conjugado = num2.conjugate()
-            numerador = num1 * denominador_conjugado
-            denominador = num2 * denominador_conjugado
+style = ttk.Style()
+style.configure("TLabel",
+                padding=10,
+                font=("Arial", 12))
 
-            resultado = numerador / denominador
-            print("Resultado da divisão:", resultado)
+style.configure("TEntry",
+                padding=10,
+                font=("Arial", 12))
 
-        elif escolha == "5":
-            sair = True
-            print("Obrigado por usar a calculadora de números complexos!")
+style.configure("TButton",
+                background="#8ecae6",
+                padding=10,
+                relief="raised",
+                font=("Arial", 12, "bold"))
 
-        else:
-            print("Opção inválida. Por favor, selecione uma opção válida.")
+frame = ttk.Frame(window, padding=20)
+frame.grid(row=0, column=0, sticky="nsew")
 
-if __name__ == "__main__":
-    main()
+label_real1 = ttk.Label(frame, text="Parte real do primeiro número:")
+label_real1.grid(row=0, column=0, sticky="w")
+entry_real1 = ttk.Entry(frame)
+entry_real1.grid(row=0, column=1)
+
+label_imag1 = ttk.Label(frame, text="Parte imaginária do primeiro número:")
+label_imag1.grid(row=1, column=0, sticky="w")
+entry_imag1 = ttk.Entry(frame)
+entry_imag1.grid(row=1, column=1)
+
+label_real2 = ttk.Label(frame, text="Parte real do segundo número:")
+label_real2.grid(row=2, column=0, sticky="w")
+entry_real2 = ttk.Entry(frame)
+entry_real2.grid(row=2, column=1)
+
+label_imag2 = ttk.Label(frame, text="Parte imaginária do segundo número:")
+label_imag2.grid(row=3, column=0, sticky="w")
+entry_imag2 = ttk.Entry(frame)
+entry_imag2.grid(row=3, column=1)
+
+button_adicao = ttk.Button(frame, text="Adição", command=somar)
+button_adicao.grid(row=4, column=0, pady=10, sticky="we")
+
+button_subtracao = ttk.Button(frame, text="Subtração", command=subtrair)
+button_subtracao.grid(row=4, column=1, pady=10, sticky="we")
+
+button_multiplicacao = ttk.Button(frame, text="Multiplicação", command=multiplicar)
+button_multiplicacao.grid(row=5, column=0, pady=10, sticky="we")
+
+button_divisao = ttk.Button(frame, text="Divisão", command=dividir)
+button_divisao.grid(row=5, column=1, pady=10, sticky="we")
+
+window.columnconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
+
+frame.columnconfigure(1, weight=1)
+frame.rowconfigure(4, weight=1)
+
+window.mainloop()
